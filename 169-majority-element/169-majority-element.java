67b9c9c9b741ed15e09c71213bfba036fe8c2c77
Shah Habibul Imran <imran15-10627@diu.edu.bd>
2022-09-22 00:41:32 +0600

    Time: 11 ms (38.32%), Space: 57.1 MB (8.77%) - LeetHub

---
+++ 169-majority-element/169-majority-element.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count = 1, position = 0;
+        
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[position]) {
+                count++;
+            } else {
+                count--;
+                
+                if (count == 0) {
+                    count = 1;
+                    position = i;
+                }
+            }
+        }
+        
+        return nums[position];
+    }
+}
