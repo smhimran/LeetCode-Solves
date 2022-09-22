cbe5fd440c035b9dfa56cf3f33da5f6ccddd8646
Shah Habibul Imran <imran15-10627@diu.edu.bd>
2022-09-22 21:56:16 +0600

    Time: 78 ms (27.06%), Space: 117.7 MB (17.06%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.java
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.java
@@ -2,14 +2,10 @@ class Solution {
     public String reverseWords(String s) {
         String ret = ""; 
         
-        for (String word: s.split(" ")) {
-            if (ret.length() > 0) {
-                ret += " ";
-            }
-            
-            ret += new StringBuilder(word).reverse().toString();
+        for (String word: s.split(" ")) {            
+            ret += new StringBuilder(word).reverse().toString() + " ";
         }
         
-        return ret;
+        return ret.trim();
     }   
 }
