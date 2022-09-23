04c68fd809c413b064151cfd5459337eb1128d5a
Shah Habibul Imran <imran15-10627@diu.edu.bd>
2022-09-23 17:12:15 +0600

    Time: 33 ms (93.48%), Space: 5.9 MB (82.61%) - LeetHub

--- 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int concatenatedBinary(int n) {
-        long result = 1;
-        int mod = (int) 1e9 + 7;
+        long result = 0;
+        int mod = 1000000007;
         int numberOfBits = 2;
         
-        for (int i = 2; i <= n; i++) {
+        for (int i = 1; i <= n; i++) {
             if (i == (1 << numberOfBits)) {
                 numberOfBits++;
             }
