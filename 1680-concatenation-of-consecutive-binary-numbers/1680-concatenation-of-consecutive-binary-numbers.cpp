da1e092dc61e1a8d039ab42c93db34160e6affea
Shah Habibul Imran <imran15-10627@diu.edu.bd>
2022-09-23 17:33:58 +0600

    Time: 24 ms (100.00%), Space: 5.9 MB (82.61%) - LeetHub

--- 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
@@ -3,17 +3,15 @@ public:
     int concatenatedBinary(int n) {
         long result = 1L;
         int numberOfBits = 1;
-        
         for (int i = 2; i <= n; i++) {
-            if (i == (1 << numberOfBits)) 
+            if (i == (1 << numberOfBits)) {
                 numberOfBits++;
-            
-            result = (result << numberOfBits) + i;
-            
-            if (result >= 1000000007) 
+            }            
+            result = (result << numberOfBits) + i;            
+            if (result >= 1000000007) {
                 result = result - (result / 1000000007) * 1000000007;
-        }
-        
+            }
+        }        
         return result;
     }
 };
