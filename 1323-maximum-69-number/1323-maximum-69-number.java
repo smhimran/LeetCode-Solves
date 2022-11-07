5531e240c9a2f80f0a1341e465972ecbd53c1610
Shah Habibul Imran <imran15-10627@diu.edu.bd>
2022-11-07 23:32:30 +0600

    Time: 0 ms (100.00%), Space: 41.6 MB (13.13%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.java
+++ 1323-maximum-69-number/1323-maximum-69-number.java
@@ -1,14 +1,15 @@
 class Solution {
     public int maximum69Number (int num) {
-        StringBuilder numString = new StringBuilder("" + num);
-
-        for (int i = 0; i < numString.length(); i++) {
-            if (numString.charAt(i) == '6') {
-                numString.setCharAt(i, '9');
-                break;
-            }
+        if (num / 1000 == 6) {
+            return num + 3000;
+        } else if ((num % 1000) / 100 == 6) {
+            return num + 300;
+        } else if ((num % 100) / 10 == 6) {
+            return num + 30;
+        } else if (num % 10 == 6) {
+            return num + 3;
+        } else {
+            return num;
         }
-
-        return Integer.valueOf(numString.toString());
     }
 }
