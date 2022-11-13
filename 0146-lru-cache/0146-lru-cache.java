6fab4319379ea9e08b42090e7ecd8e00c1e5f1f8
Shah Habibul Imran <imran15-10627@diu.edu.bd>
2022-11-14 01:09:24 +0600

    Time: 144 ms (13.14%), Space: 132.4 MB (28.66%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,20 +1,80 @@
 class LRUCache {
-    Map<Integer, Integer> store;
+    Map<Integer, Node> store;
+    Node left;
+    Node right;
+    int capacity;
+    int numberOfElements;
 
     public LRUCache(int capacity) {
-        store = new LinkedHashMap<Integer, Integer>(capacity, 0.75f, true) {
-            protected boolean removeEldestEntry(Map.Entry entry) {
-                return size() > capacity;
-            }
-        };
+        store = new HashMap<>();
+        this.capacity = capacity;
+        this.left = new Node(0, 0);
+        this.right = new Node(0, 0);
+        left.next = right;
+        right.prev = left;
+        numberOfElements = 0;
     }
     
     public int get(int key) {
-        return store.getOrDefault(key, -1);
+        if (store.containsKey(key)) {
+            Node node = store.get(key);
+            evict(node);
+            addNode(node);
+            return node.value;
+        } else {
+            return -1;
+        }
     }    
     
     public void put(int key, int value) {
-        store.put(key, value);
+        if (store.containsKey(key)) {
+            Node node = store.get(key);
+            evict(node);
+            node.value = value;
+            addNode(node);
+        } else {
+            Node node = new Node(key, value);
+            addNode(node);
+
+            if (numberOfElements < capacity) {
+                numberOfElements++;
+            } else {
+               evict();
+            }
+        }
+    }
+
+    private void evict() {
+        store.remove(left.next.key);
+        left.next = left.next.next;
+        left.next.prev = left;
+    }
+
+    private void evict(Node node) {
+        store.remove(node.key);
+        Node prev = node.prev;
+        Node next = node.next;
+        prev.next = next;
+        next.prev = prev;
+    }
+
+    private void addNode(Node node) {
+        node.prev = right.prev;
+        right.prev.next = node;
+        node.next = right;
+        right.prev = node;
+
+        store.put(node.key, node);
+    }
+}
+
+class Node {
+    int key, value;
+    Node prev, next;
+
+    public Node(int key, int value) {
+        this.key = key;
+        this.value = value;
     }
 }
 
