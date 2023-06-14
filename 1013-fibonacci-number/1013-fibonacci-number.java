c7414f9762f58a3e471b91ba1ecba5d39b33e54e
Shah Habibul Imran <imran15-10627@diu.edu.bd>
2023-06-14 19:19:39 +0600

    Time: 0 ms (100%), Space: 39.2 MB (86.88%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,9 +1,17 @@
 class Solution {
+    int[] dp = new int[35];
+
     public int fib(int n) {
         if (n == 0 || n == 1) {
             return n;
         }
 
-        return fib(n - 1) + fib(n - 2);
+        if (dp[n] != 0) {
+            return dp[n];
+        }
+
+        dp[n] = fib(n - 1) + fib(n - 2);
+
+        return dp[n];
     }
 }
