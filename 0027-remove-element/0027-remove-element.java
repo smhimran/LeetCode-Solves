a87a4c62cbaf3ebf0e8ddb719fefa0e97a7e92e5
Shah Habibul Imran <imran15-10627@diu.edu.bd>
2023-06-19 22:41:19 +0600

    Time: 0 ms (100%), Space: 41.4 MB (33.04%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -2,19 +2,20 @@ class Solution {
     public int removeElement(int[] nums, int val) {
         int next = 0;
         int now = 0;
-        int numberOfNonVal = 0;
 
+        // Find first index with val
         while (now < nums.length && nums[now] != val) {
             now++;
-            numberOfNonVal++;
         }
         
         next = now;
 
+        // Find first index without val after 'now'
         while (next < nums.length && nums[next] == val) {
             next++;
         }
 
+        // For each index, find next non val, swap with now, and increment now by one
         for (; next < nums.length; next++) {
             if (nums[next] == val) {
                continue;
@@ -22,11 +23,10 @@ class Solution {
 
             nums[now] = nums[next];
             nums[next] = val;
-            
-            numberOfNonVal++;
+
             now++;
         }
 
-        return numberOfNonVal;
+        return now;
     }
 }
