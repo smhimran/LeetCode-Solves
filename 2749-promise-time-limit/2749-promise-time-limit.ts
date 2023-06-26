f84323d038df786dc074b5f56a5f47ba6ed79be6
Shah Habibul Imran <imran15-10627@diu.edu.bd>
2023-06-26 19:50:56 +0600

    Time: 66 ms (58.35%), Space: 42.2 MB (99.67%) - LeetHub

--- 2749-promise-time-limit/2749-promise-time-limit.ts
+++ 2749-promise-time-limit/2749-promise-time-limit.ts
@@ -2,15 +2,12 @@ type Fn = (...params: any[]) => Promise<any>;
 
 function timeLimit(fn: Fn, t: number): Fn {
 	return async function(...args) {
-        const promise = fn(...args);
-
-        const timeoutPromise = new Promise((resolve, reject) => {
-        setTimeout(() => {
-                reject("Time Limit Exceeded");
-            }, t);
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                    reject("Time Limit Exceeded");
+                }, t);
+            fn(...args).then(resolve, reject);
         });
-
-        return Promise.race([promise, timeoutPromise]);
     }
 };
 
