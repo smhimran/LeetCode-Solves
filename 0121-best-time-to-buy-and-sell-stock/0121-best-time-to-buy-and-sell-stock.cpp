7c947a882d520380e3d473fe7db1618f433722db
Shah Habibul Imran <smhimran@gmail.com>
2024-11-20 19:48:28 +0600

    Time: 0 ms (100%), Space: 96.1 MB (72.27%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int buyPrice = prices[0];
+        int profit = 0;
+
+        for (int i = 1; i < prices.size(); i++) {
+            if (prices[i] < buyPrice) {
+                buyPrice = prices[i];
+            } else if (prices[i] > buyPrice) {
+                profit = max(profit, prices[i] - buyPrice);
+            }
+        }
+
+        return profit;
+    }
+};
