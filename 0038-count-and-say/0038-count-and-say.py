0c6df66657388feda96b227e718b81302886820f
Shah Habibul Imran <smhimran@gmail.com>
2024-11-24 00:18:00 +0600

    Time: 3 ms (97.94%), Space: 16.5 MB (88.78%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.py
@@ -0,0 +1,27 @@
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        if n == 1:
+            return "1"
+        
+        return self.generateRLE(self.countAndSay(n - 1))
+
+
+    def generateRLE(self, number: str) -> str:
+        rle = ""
+
+        if not number:
+            return rle
+        
+        count = 1
+        digit = number[0]
+
+        for i in range(1, len(number)):
+            if number[i] == number[i - 1]:
+                count += 1
+            else:
+                rle += f"{count}{digit}"
+                count = 1
+                digit = number[i]
+
+        rle += f"{count}{digit}"
+        return rle
